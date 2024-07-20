54e4014f5eb0a042bfb705e0641dbcd40b5a51ed
kokwei <kokweikhong@gmail.com>
2024-07-20 22:35:29 +0800

    updates

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -11,15 +11,19 @@ return {
   -- lazy = false,
   -- priority = 1000,
   -- opts = {},
-  "catppuccin/nvim",
-  name = "catppuccin",
+  -- "catppuccin/nvim",
+  -- name = "catppuccin",
+  -- priority = 1000,
+  "ellisonleao/gruvbox.nvim",
   priority = 1000,
   config = function()
-    -- require("tokyonight").setup()
-    require("catppuccin").setup()
-
-    -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
-    vim.cmd("colorscheme catppuccin-macchiato")
+    require("gruvbox").setup()
+    vim.cmd("colorscheme gruvbox")
+    --   -- require("tokyonight").setup()
+    --   require("catppuccin").setup()
+    --
+    --   -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
+    --   vim.cmd("colorscheme catppuccin-macchiato")
     -- ColorMyPencils()
   end,
 }
