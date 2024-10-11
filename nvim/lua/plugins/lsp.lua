790814d3affd5f96348c2096b86406cef6ca6ced
kokwei <kokweikhong@gmail.com>
2024-10-11 13:10:12 +0800

    updates

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -18,8 +18,8 @@ return {
     -- Automatically install LSPs to stdpath for neovim
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
-    "hrsh7th/nvim-cmp", -- Autocompletion plugin
-    "hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
+    "hrsh7th/nvim-cmp",         -- Autocompletion plugin
+    "hrsh7th/cmp-nvim-lsp",     -- LSP source for nvim-cmp
     "saadparwaiz1/cmp_luasnip", -- Snippets source for nvim-cmp
     "L3MON4D3/LuaSnip",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
@@ -140,6 +140,13 @@ return {
 
     -- nvim-cmp setup
     local cmp = require("cmp")
+
+    local has_words_before = function()
+      unpack = unpack or table.unpack
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    end
+
     cmp.setup({
       snippet = {
         expand = function(args)
@@ -148,9 +155,29 @@ return {
       },
       mapping = cmp.mapping.preset.insert({
         ["<C-u>"] = cmp.mapping.scroll_docs(-4), -- Up
-        ["<C-d>"] = cmp.mapping.scroll_docs(4), -- Down
+        ["<C-d>"] = cmp.mapping.scroll_docs(4),  -- Down
         -- C-b (back) C-f (forward) for snippet placeholder navigation.
-        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        -- ["<C-Space>"] = cmp.mapping.complete(),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            if #cmp.get_entries() == 1 then
+              cmp.confirm({ select = true })
+            else
+              cmp.select_next_item()
+            end
+            --[[ Replace with your snippet engine (see above sections on this page)
+      elseif snippy.can_expand_or_advance() then
+        snippy.expand_or_advance() ]]
+          elseif has_words_before() then
+            cmp.complete()
+            if #cmp.get_entries() == 1 then
+              cmp.confirm({ select = true })
+            end
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
         ["<CR>"] = cmp.mapping.confirm({
           behavior = cmp.ConfirmBehavior.Replace,
           select = true,
